4310cb19ac156b34d78fff72ef41e7fce4556420
Andrey Dorokhov <andrew@dorohoff.net>
2014-08-23 15:42:21 +0300

    Added styled 404 page

--- RabbitCMS/admin/not-found.php
+++ RabbitCMS/admin/not-found.php
@@ -1,2 +1,29 @@
 <?php defined('_RabbitCMS') or die('Restricted access'); ?>
-Page not found. Sorry. :(
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>404 Page not found</title>
+
+    <!-- Bootstrap -->
+    <link href="/admin/css/bootstrap.min.css" rel="stylesheet">
+    <style type="text/css">
+        body {padding-top: 50px;}
+    </style>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="container">
+        <h1>404 <small>Nothing to see here</small></h1>
+        <p>You've stumbled across a page that does not exist. Check you have the correct URL or return to the <a href="/">homepage</a>.</p>
+    </div>
+  </body>
+</html>
